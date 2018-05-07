fa9639771b5de267f7b836118ab4fc7f087cc937
Diego Sierra Fernandez <diegosierr28@hotmail.com>
2018-05-07 11:31:19 +0200

    Add files via upload

--- blog_api/app/models/article.rb
+++ blog_api/app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
 	has_many :comments
+	validates :text, presence: true, length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 1 }
 end
--- blog_api/app/models/comment.rb
+++ blog_api/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article, optional: true
+  validates :body, presence: true, length: { minimum: 5 }
+  validates :commenter, presence: true, length: { minimum: 1 }
 end
